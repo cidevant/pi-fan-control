8766c5613d475799aea2b41020fca7983cb40777
Cidevant Von Goethe <cidevant@mail.ru>
2022-06-03 12:29:17 +0200

    feat: install script

--- install.sh
+++ install.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-cd "$(dirname "$0")/.."
-
 sudo apt install python3-lgpio python3-rpi.gpio
 
 echo "=> Installing fan controller...\n"
-sudo cp fancontrol.py /usr/local/bin/
+sudo cp ./fancontrol.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/fancontrol.py
 
+
 echo "=> Starting fan controller...\n"
-sudo cp fancontrol.sh /etc/init.d/
+sudo cp ./fancontrol.sh /etc/init.d/
 sudo chmod +x /etc/init.d/fancontrol.sh
 
 sudo update-rc.d fancontrol.sh defaults
